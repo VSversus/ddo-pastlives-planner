abb206c183fb05f6317590e45f4dc01ed4ece3d5
Václav Schneider <vaclav.schneider@drmax.eu>
2022-11-06 02:11:51 +0100

    import export working

--- importExport.js
+++ importExport.js
@@ -2,19 +2,47 @@
 
 function exportList() {
     let csvContent = "data:text/csv;charset=utf-8,";
+    csvContent += 'Number,Name\r\n';
     let selectionsArray = document.getElementsByClassName('reincarnationSelect')
     for (let i = 0; i < selectionsArray.length; i++) {
         let nameOfClass = selectionsArray[i].selectedOptions[0].label;
-        csvContent += i+1 + ';' + nameOfClass + "\r\n";
+        csvContent += i+1 + ',' + nameOfClass + "\r\n";
     };
     var encodedUri = encodeURI(csvContent);
     window.open(encodedUri);
 }
 
-function importList() {
-    for (let index = 0; index < array.length; index++) {
-        //const element = array[index];
-        document.getElementById('reincarnation' + i).value = nameOfClass;
+function createEventListenerForImport() {
+    const fileSelector = document.getElementById('fileSelector');
+    fileSelector.addEventListener('change', () => {
+        const [file] = fileSelector.files;
+        const reader = new FileReader();
+
+        reader.addEventListener("load", () => {
+            // this will then display a text file
+            const importArray = d3.csvParse(reader.result);
+            importList(importArray);
+        }, false);
+
+        if (file) {
+          reader.readAsText(file);
+        }
+    });
+}
+
+function importList(importArray) {
+    for (let i = 0; i < importArray.length; i++) {
+        if (importArray[i].Name != '-') {
+            document.getElementById('reincarnation' + importArray[i].Number).value = importArray[i].Name;
+        } else {
+            continue
+        }
     }
+    saveReincarnatedClasses();
+    calculateReincarnations();
+    printFeatsCalculation();
+    checkMaximumNumberOfReincarnationsForOneClass();
     changeSelectColorForDefaultValue();
 }
+
+createEventListenerForImport();
